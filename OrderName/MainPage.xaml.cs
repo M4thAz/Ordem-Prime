00317bd7a9ce87e908c897847db9d7a87e628f4c
Matheus Azevedo <matheus.kwt2@hotmail.com>
2023-09-26 18:33:35 +0100

    atualização do projeto de aula

--- OrderName/MainPage.xaml.cs
+++ OrderName/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrderName.Pages;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -17,7 +18,25 @@ namespace OrderName
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Seja bem vindo!", "Digite seu nome!", "Aguarde ser chamado!");
+            DisplayAlert("Seja bem vindo! ", "Aguarde ser chamado " + TXTNome.Text + "!", "Confirmar");
+        }
+
+        private void BTN_ROW_Clicked(object sender, EventArgs e)
+        {
+            //transforma o evento do botão para seguir a outra página
+            Navigation.PushAsync(new Pages.Row());
+        }
+
+        private void BTNEventTest_Pressed(object sender, EventArgs e)
+        {
+            //puxa o evento da label passando um texto enquanto segura o botao
+            LbEvent.Text = "Holding!";
+        }
+
+        private void BTNEventTest_Released(object sender, EventArgs e)
+        {
+            //puxa o evento da label quando o botão é solto
+            LbEvent.Text = "Umpressed";
         }
     }
 }
---
+++ OrderName/Pages/Row.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrderName.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Row : ContentPage
+	{
+		public Row ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
