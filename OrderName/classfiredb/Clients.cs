a58bbdece9164e106b5072475ccfdd38cb49503d
Matheus Azevedo <108142400+M4thAz@users.noreply.github.com>
2024-01-22 12:15:02 +0000

    Update Clients.cs

--- OrderName/classfiredb/Clients.cs
+++ OrderName/classfiredb/Clients.cs
@@ -4,10 +4,9 @@ using System.Text;
 
 namespace OrderName.classfiredb
 {
+     //criação de campos na base de dados
     public class Clients
     {
         public string User { get; set; }
-        //public int IdClient { get;  set; }
-
     }
 }
